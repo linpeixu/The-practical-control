fc62f4fd95b8c7089f529a7029fa91c6126c7d89
云和煦 <1966353889@qq.com>
2018-08-14 09:56:50 +0800

    测试提交

--- app/src/main/java/com/cax/rxjava/MainActivity.java
+++ app/src/main/java/com/cax/rxjava/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         test();
+        //测试文本
     }
 
     @Override
